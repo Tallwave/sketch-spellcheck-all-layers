57fab3b3be8192bcfde5faa3d237e5e6aaeae92d
Sean Rice <sean.rice@ethology.com>
2017-01-09 17:11:11 -0700

    Lots of mess right now!

--- SpellCheckWholePage.sketchplugin/Contents/Sketch/spellcheckwholepage.js
+++ SpellCheckWholePage.sketchplugin/Contents/Sketch/spellcheckwholepage.js
@@ -4,8 +4,8 @@ function onRun(context) {
   var sketch = context.api();
   var doc = context.document;
 
-  var documentTag = [[NSSpellChecker sharedSpellChecker] uniqueSpellDocumentTag]
-
+  var documentTag = [NSSpellChecker uniqueSpellDocumentTag]
+  var language = [[NSSpellChecker sharedSpellChecker] language]
   // Filter layers using NSPredicate
 	var scope = (typeof containerLayer !== 'undefined') ? [containerLayer children] : [[doc currentPage] children],
 		predicate = NSPredicate.predicateWithFormat("(className == %@)", "MSTextLayer"),
@@ -19,39 +19,47 @@ function onRun(context) {
   var allWords = "";
   var misspellingcount = 0;
   var stopChecking = false;
-  while (layer = [loop nextObject] &! stopChecking) {
+  var notReady = true;
+  while (layer = [loop nextObject]) {
+    if(stopChecking){
+      break;
+    }
+    //while (notReady){
+
+
     //do spellcheck on each layer
-        var aString = [layer stringValue]
-        range = [[NSSpellChecker sharedSpellChecker] checkSpellingOfString:aString startingAt:0];
-        if(range.length >0){
-          //Select the layer
-          //[layer select:true byExpandingSelection:true]
-          [layer select:true byExpandingSelection:false]
+    var holdItOpen = true;
+    var aString = [layer stringValue]
+    range = [[NSSpellChecker sharedSpellChecker] checkSpellingOfString:aString startingAt:0];
+    if(range.length >0){
+      //Select the layer
+      //[layer select:true byExpandingSelection:true]
+      [layer select:true byExpandingSelection:false]
 
-          var misSpelledWord = aString.substring(range.location, (range.location+range.length))
-          allWords = allWords+"\nText: "+aString+"\nMisspelled Word: "+misSpelledWord+"\n";
-          misspellingcount ++;
-          var guesses = [[NSSpellChecker sharedSpellChecker] guessesForWordRange: range inString: aString ];
-          //[[NSSpellChecker sharedSpellChecker] updateSpellingPanelWithMisspelledWord:misSpelledWord] // Updates the spell checker window with the misspelled word. Since we can't update the text yet, this isn't helpful, so it's commented out.
+      var misSpelledWord = aString.substring(range.location, (range.location+range.length))
+      allWords = allWords+"\nText: "+aString+"\nMisspelled Word: "+misSpelledWord+"\n";
+      misspellingcount ++;
+      var guesses = [[NSSpellChecker sharedSpellChecker] guessesForWordRange: range inString: aString language: language inSpellDocumentWithTag: documentTag ];
+      //[[NSSpellChecker sharedSpellChecker] updateSpellingPanelWithMisspelledWord:misSpelledWord] // Updates the spell checker window with the misspelled word. Since we can't update the text yet, this isn't helpful, so it's commented out.
 
-          //NOTE: There's a possibility we could use the getSelectionFromUser method from the Sketch Javascript API to give a list of options such as "skip, add to dictionary, replace with..."
-          //Documetntation here: http://developer.sketchapp.com/reference/api/class/api/Application.js~Application.html#instance-method-alert
+      //NOTE: There's a possibility we could use the getSelectionFromUser method from the Sketch Javascript API to give a list of options such as "skip, add to dictionary, replace with..."
+      //Documetntation here: http://developer.sketchapp.com/reference/api/class/api/Application.js~Application.html#instance-method-alert
 
-          var alert = NSAlert.alloc().init();
+      var alert = NSAlert.alloc().init();
 
-          alert.setMessageText('Alert Title');
-          //alert.setInformativeText('Alert Text');
-          //alert.addButtonWithTitle('OK');
-          //alert.addButtonWithTitle('Cancel');
-          //alert.setIcon(NSImage.alloc().initWithContentsOfFile(
-          //    context.plugin.urlForResourceNamed('DialogIcon512.png').path()));
+      alert.setMessageText('Alert Title');
+      //alert.setInformativeText('Alert Text');
+      //alert.addButtonWithTitle('OK');
+      //alert.addButtonWithTitle('Cancel');
+      //alert.setIcon(NSImage.alloc().initWithContentsOfFile(
+      //    context.plugin.urlForResourceNamed('DialogIcon512.png').path()));
 
-          var nibui = new NibUI(context,
-              'UIBundle', 'MyNibUI',
-              ['textMisSpelling', 'replaceComboBox', 'btnSkip', 'btnReplace','btnIgnoreAll','btnAddDict','btnDone','textFullText']);
+      var nibui = new NibUI(context,
+          'UIBundle', 'SpellCheckWholePage',
+          ['textMisSpelling', 'replaceComboBox', 'btnSkip', 'btnReplace','btnIgnoreAll','btnAddDict','btnDone','textFullText']);
 
-          alert.setAccessoryView(nibui.view);
-
+      alert.setAccessoryView(nibui.view);
+/*
           //Set up our text
           nibui.textMisSpelling.stringValue = "Mispelling: "+ misSpelledWord;
           nibui.textFullText.stringValue = aString;
@@ -64,7 +72,7 @@ function onRun(context) {
             //context.document.showMessage('hey');
             //Do text replace
             layer.setIsEditingText(true);
-            layer.stringValue = aString.replace( misSpelledWord, nibui.replaceComboBox.objectValueOfSelectedItem);
+            [layer stringValue] = aString.replace( misSpelledWord, nibui.replaceComboBox.objectValueOfSelectedItem);
             layer.setIsEditingText(false);
           });
 
@@ -83,7 +91,7 @@ function onRun(context) {
             // Next!
           });
 
-          nibui.attachTargetAndAction(nibui.addDict, function() {
+          nibui.attachTargetAndAction(nibui.btnAddDict, function() {
             // Add the word to the Dictionary.
             // Use the NSSpellchecker method.
             [[NSSpellChecker sharedSpellChecker] learnWord: misSpelledWord]
@@ -92,11 +100,13 @@ function onRun(context) {
           alert.runModal();
 
           nibui.destroy();
+          */
         }
+      //}
 
 	}
   //Builds our little alert
   //allWords = allWords + "\nFound "+misspellingcount+" misspellings in "+[layers count]+" text layers";
   //[[NSApplication sharedApplication] displayDialog:allWords withTitle:"Spellcheck Whole Page"]
-  sketch.alert("Found "+misspellingcount+" misspellings in "+[layers count]+" text layers", allWords)
+  //sketch.alert("Found "+misspellingcount+" misspellings in "+[layers count]+" text layers", allWords)
 }
